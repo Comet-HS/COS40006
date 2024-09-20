c7fa11cef9c08c135b21c56fcec35bdd6cbb903b
Henry-Sutton <139392084+Comet-HS@users.noreply.github.com>
2024-09-20 13:39:49 +1000

    various improvements
    Changed aiko installation in pyproject.toml
    reorganised pipelines and elements to their respective directories
    fixed emotion_detection_element to now work. Still uses very simple detection WIP.

--- src/cos40006/elements/emotion_detection_element.py
+++ src/cos40006/elements/emotion_detection_element.py
@@ -1,15 +1,23 @@
 import aiko_services as aiko
 from typing import Tuple
 
+ # Run test with frame data
+ # aiko_pipeline create emotion_pipeline.json --frame_data "(emotion_text: 'I am very happy today!')"
+
+ # Can't load module?
+ # 
+
+ # To Do
+ # once working, correct file paths to appropriate directory.
+
 
 class EmotionDetectionElement(aiko.PipelineElement):
     def __init__(self, context):
         context.set_protocol("emotion:0")
         context.get_implementation("PipelineElement").__init__(self, context)
 
-    def process_frame(self, stream, frame) -> Tuple[aiko.StreamEvent, dict]:
-        # Extract emotion_text from the frame
-        emotion_text = frame.get("emotion_text", "")
+    def process_frame(self, stream, emotion_text) -> Tuple[aiko.StreamEvent, dict]:
+
         self.logger.debug(f"received text: {emotion_text}")
 
         # Detect the emotion
